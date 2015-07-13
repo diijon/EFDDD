bb6edab8b2249bd1446d8e50f1d0a2e66932b01e
diijon <diijon@gmail.com>
2015-07-13 09:39:03 -0400

    removed deprecated logging class

--- src/DataModel.Migrations/Core/Contracts/IMigrationRunnerLog.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EFDDD.DataModel.Migrations.Core.Contracts
-{
-    public interface IMigrationRunnerLog
-    {
-        void LogError(string message);
-        void LogInfo(string message);
-        void LogWarning(string message);
-    }
-}
